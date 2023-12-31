0fdcac50bdbf9d3444dfb9c29e61b0b3a07d84e3
Emanuele Muzio <emanuele.muzio@techedgegroup.com>
2023-12-31 19:44:11 +0100

    wip calibration, 2/3, TODO: delete opencv implementation

---
+++ calibrazione/dlt.py
@@ -0,0 +1,170 @@
+from scipy import linalg
+import numpy as np
+import cv2 as cv
+from sklearn import preprocessing
+
+# Definiamo i punti dell'oggetto usando coordinate 3D "false", usando un sistema che ha origine da un angolo del cubo, e i punti dell'immagine, misurati manualmente
+
+def getPoints(points):
+    objp = np.zeros((points, 3), np.float32)
+    imgp = np.zeros((points,2), np.float32)
+    
+        
+    if points >= 6:
+        
+        # objp[0,:] sarà uguale all'origine del sistema di riferimento che piazziamo sul cubo
+        objp[1,:] =[6, 0, 1]
+        objp[2,:] =[7, 0, 7]
+        objp[3,:] =[7, 7, 7]
+        objp[4,:] =[3, 0, 4]
+        objp[5,:] =[0, 7, 0]
+        
+        imgp[0,:] = [103, 254]
+        imgp[1,:] = [253, 189]
+        imgp[2,:] = [258, 39]
+        imgp[3,:] = [155, 10]
+        imgp[4,:] = [173, 132]
+        imgp[5,:] = [18, 196]
+        
+        if points >= 12:
+            
+            objp[6,:] =[0, 7, 7]
+            objp[7,:] =[0, 4, 4]
+            objp[8,:] =[0, 0, 7]
+            objp[9,:] =[4, 4, 7]
+            objp[10,:] =[7, 0, 3]
+            objp[11,:] =[0, 3, 7]
+            
+            
+            imgp[6,:] = [14, 26]
+            imgp[7,:] = [50, 120]
+            imgp[8,:] = [103, 61]
+            imgp[9,:] = [136, 29]
+            imgp[10,:] = [254, 141]
+            imgp[11,:] = [62, 46]
+            
+            if points == 24:
+                
+                objp[12,:] =[0, 7, 3]
+                objp[13,:] =[7, 2, 7] 
+                objp[14,:] =[1, 0, 1]   
+                objp[15,:] =[2, 0, 1]   
+                objp[16,:] =[3, 0, 1]   
+                objp[17,:] =[4, 0, 1]   
+                objp[18,:] =[5, 0, 1]   
+                objp[19,:] =[6, 0, 1]   
+                objp[20,:] =[1, 0, 2]   
+                objp[21,:] =[2, 0, 2]   
+                objp[22,:] =[3, 0, 2]   
+                objp[23,:] =[4, 0, 2]   
+                
+                imgp[12,:] = [16, 127]
+                imgp[13,:] = [224, 27]
+                imgp[14,:] = [127, 221]
+                imgp[15,:] = [149, 216]
+                imgp[16,:] = [171, 210]
+                imgp[17,:] = [193, 205]
+                imgp[18,:] = [213, 199]
+                imgp[19,:] = [234, 193]
+                imgp[20,:] = [127, 194]
+                imgp[21,:] = [149, 190]
+                imgp[22,:] = [172, 184]
+                imgp[23,:] = [193, 179]
+    
+    return objp, imgp
+
+# Facciamo una stima della matrice di camera P a partire dalle corrispondenze tra punti dell'oggetto e dell'immagine
+
+def estimateCameraMatrix(objp, imgp, points):
+    A = np.zeros((points * 2, 12), dtype = np.float32)
+    X, Y, Z = 0, 1, 2
+    
+    # Se un punto dell'immagine è uguale a un punto 3D per la matrice di proiezione che stiamo cercando P, allora possiamo trattare il sistema di equazioni riscritte come un problema di minimizzazione Ap = 0, per cui possiamo vincolare ||p||^2  = 1 e cercare un p tale che Ap si avvicini quanto più possibile a 0 e la norma di p si avvicini il più possibile a 1. Il fulcro della nostra ricerca è un problema agli autovalori, dal momento che vogliamo ricercare tramite SVD il valore singolare i-esimo più piccolo, in modo da, se la decomposizione ottenuta tramite SVD è uguale a M = USV^t, l'i-esimo vettore di V. Questa sarà la nostra matrice di proiezione 3x4.
+    
+    for i in range(points):
+        A[2 * i:] = np.array([
+                objp[i,X],
+                objp[i,Y], 
+                objp[i,Z], 
+                1, 
+                0, 0, 0, 0, 
+                -imgp[i,X] * objp[i,X], 
+                -imgp[i,X] * objp[i,Y], 
+                -imgp[i,X] * objp[i,Z], 
+                imgp[i,X]
+        ])
+         
+        A[2 * i + 1:] = np.array(
+            [
+                0, 0, 0, 0,
+                objp[i,X],
+                objp[i,Y], 
+                objp[i,Z], 
+                1,
+                -imgp[i,Y] * objp[i,X], 
+                -imgp[i,Y] * objp[i,Y], 
+                -imgp[i,Y] * objp[i,Z], 
+                imgp[i,Y]  
+            ]
+        )
+        
+    U, S, Vt = np.linalg.svd(A)
+    
+    P = Vt[np.argmin(S)]
+    
+    return P.reshape(3, 4)
+    
+def drawCubePoints(CUBE, imgp):
+    for p in imgp:
+        CUBE = cv.circle(CUBE, p.astype(np.int32), 3, (255, 0, 0))
+    cv.imshow("CUBE", CUBE)
+    cv.waitKey(0)
+    cv.destroyAllWindows()    
+    
+# drawCubePoints(CUBE, imgp)
+
+def getKR(P):
+    K, R = linalg.rq(P[:3,:3])
+    
+    # Normalizziamo K in modo da avere 1 in posizione 3:3
+    # Se non facciamo questo possiamo riscontrare dei problemi con l'inversione di K allo step successivo
+    
+    K = K/K[2,2]    
+    
+    return K, R
+
+def getT(K, P):
+    t = np.matmul(
+        np.linalg.inv(K), P[:,3]
+    )  
+    
+    return t
+
+def guess_K(shape):
+    
+    # initial guess of K
+    
+    K = np.zeros((3,3))
+
+    K[0,0]=500
+    K[1,1]=500
+    K[2,2]=1
+    K[0,2]=shape[0]/2
+    K[1,2]=shape[1]/2
+    return K
+
+# K_guess = guess_K(CUBE.shape)
+
+# OPENCV_ret, OPENCV_K, OPENCV_dist, OPENCV_Rs, OPENCV_ts = cv.calibrateCamera(
+#     [objp], 
+#     [imgp], 
+#     CUBE.shape[0:2], 
+#     K_guess, 
+#     None, 
+#     flags=
+#         cv.CALIB_USE_INTRINSIC_GUESS|
+#         cv.CALIB_FIX_S1_S2_S3_S4|
+#         cv.CALIB_ZERO_TANGENT_DIST|
+#         cv.CALIB_FIX_K2|
+#         cv.CALIB_FIX_K3
+# )
